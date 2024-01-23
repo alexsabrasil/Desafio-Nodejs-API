ebd16dd9b4b9d36f22cb3f228ca4d8dcacbd99db
Alexsandra Tavares <alexsa.tecnolog@gmail.com>
2024-01-23 18:43:22 -0300

    Adicionando novos recursos

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,25 @@
 // authController.js
-const bcrypt = require('bcrypt');
+const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const keys = require('../config/keys');
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
+
+// Nova senha
+const novaSenha = 'sua_nova_senha';
+
+// Número de rounds de sal
 const SALT_ROUNDS = 10;
+
+// Gerar a senha criptgrafada
+bcrypt.hash(novaSenha, SALT_ROUNDS, (err, hashedPassword) => {
+  if (err) {
+    console.error('Erro ao gerar a senha criptografada:', err);
+  } else {
+    console.log('Nova senha criptografada:', hashedPassword);
+  }
+});
 
 exports.register = async (req, res) => {
   let newUser;
@@ -43,24 +58,23 @@ exports.register = async (req, res) => {
   }
 };
 
-// Middleware de autenticação
-exports.login = async (req, res) => {
+// Middleware de autenticação local
+exports.localLogin = (req, res, next) => {
   passport.authenticate('local', { session: false }, (err, user, info) => {
-    if (err || !user) {
-      console.error('Erro durante o login:', err || info); // Log de erro
+    if (err) {
+      console.error('Erro durante o login (passport.authenticate):', err); // Log de erro
       return res.status(500).json({ message: 'Erro interno do servidor.' });
     }
 
-    req.login(user, { session: false }, (error) => {
-      if (error) {
-        console.error('Erro durante o login:', error); // Log de erro
-        res.status(500).json({ message: 'Erro interno do servidor.' });
-      }
+    if (!user) {
+      console.error('Usuário não encontrado:', info);
+      return res.status(401).json({ message: 'Credenciais inválidas.' });
+    }
 
-      const token = jwt.sign({ user }, keys.jwtSecret, {
-        expiresIn: '1h',
-      });
-      res.status(200).json({ token });
+    const token = jwt.sign({ user }, keys.jwtSecret, {
+      expiresIn: '1h',
     });
-  })(req, res);
+
+    res.status(200).json({ token });
+  })(req, res, next);
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,32 +1,38 @@
 // authRoutes.js
 const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const authController = require('../controllers/authController');
 const keys = require('../config/keys');
 
-const router = express.Router();
-
+// Rota de registro
 router.post('/register', authController.register);
 
-router.post(
-  '/login',
-  passport.authenticate('local', { session: false }),
-  (req, res) => {
-    const token = jwt.sign({ user: req.user }, keys.jwtSecret, {
+// Rota de login local
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err) {
+      console.error('Erro durante o login (passport.authenticate):', err);
+      return res.status(500).json({ message: 'Erro interno do servidor.' });
+    }
+
+    if (!user) {
+      console.error('Usuário não encontrado:', info);
+      return res.status(401).json({ message: 'Credenciais inválidas.' });
+    }
+
+    const token = jwt.sign({ user }, keys.jwtSecret, {
       expiresIn: '1h',
     });
+
     res.status(200).json({ token });
-  }
-);
+  })(req, res, next);
+});
 
 // Rota protegida
-router.get(
-  '/protected',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.status(200).json({ message: 'Rota protegida alcançada!' });
-  }
-);
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.status(200).json({ message: 'Rota protegida alcançada!' });
+});
 
 module.exports = router;
